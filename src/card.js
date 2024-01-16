dfc3464d648699978daea09fadaf4720adbec414
alexx <alexxc46@gmail.com>
2024-01-16 11:56:09 +0200

    rotates with animejs with issues

--- src/card.js
+++ src/card.js
@@ -1,27 +1,48 @@
+import anime from '../node_modules/animejs/lib/anime.es.js';
+
 class Card {
-    constructor(value, index, clickHandler) {
-      this.value = value;
-      this.index = index;
-      this.clickHandler = clickHandler;
-      this.element = this.createCardElement();
-    }
-  
-    createCardElement() {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      card.dataset.index = this.index;
-      card.textContent = '?';
-      card.addEventListener('click', () => this.clickHandler(this));
-      return card;
-    }
-  
-    reveal() {
-      this.element.textContent = this.value;
-    }
-  
-    hide() {
-      this.element.textContent = '?';
-    }
+  constructor(value, index, clickHandler) {
+    this.value = value;
+    this.index = index;
+    this.clickHandler = clickHandler;
+    this.element = this.createCardElement();
   }
-  
-  export default Card;
+
+  createCardElement() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.index = this.index;
+    card.textContent = '?';
+    
+    // Add click event listener to trigger the animation
+    card.addEventListener('click', () => this.clickHandler(this));
+
+    return card;
+  }
+
+  reveal() {
+    anime({
+      targets: this.element,
+      rotateY: 180,
+      easing: 'easeInOutQuad',
+      duration: 500,
+      complete: () => {
+        this.element.textContent = this.value;
+      },
+    });
+  }
+
+  hide() {
+    anime({
+      targets: this.element,
+      rotateY: 0,
+      easing: 'easeInOutQuad',
+      duration: 500,
+      complete: () => {
+        this.element.textContent = '?';
+      },
+    });
+  }
+}
+
+export default Card;
